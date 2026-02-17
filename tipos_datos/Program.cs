17c7b95efcffab0d2688d24a17054b397dea092f
David Alejandro Orozco Henao <davidalejandroorozcoheno@gmail.com>
2026-02-16 19:14:13 -0500

    tipos de datos

---
+++ tipos_datos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ // variables
+ int age = 25;
+ decimal height = 1.75m;
+ bool is_student = true;
+ string name = "Ana";
+ char grade = 'A';
+
+     // Impresión en consola
+     Console.WriteLine("La edad es: " + age);
+     Console.WriteLine("La estatura es: " + height);
+     Console.WriteLine("¿Es estudiante?: " + is_student);
+     Console.WriteLine("El nombre es: " + name);
+     Console.WriteLine("La calificación es: " + grade);
+    }
+ }
